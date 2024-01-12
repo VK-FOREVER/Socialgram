182a0011d9045cba849a052d6a2189270d23b13e
Aman <dv451197@gmail.com>
2024-01-12 21:08:11 +0530

    ready to push

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quidem
+        illo maiores, libero perferendis cupiditate quaerat ipsam ipsum
+        laudantium neque eos, iure, at nobis voluptatum. Quisquam quasi facilis
+        eum quaerat.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+        ducimus earum, exercitationem ratione vitae error doloribus
+        necessitatibus aperiam odit mollitia?
+      </p>
+      <span>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, sed.
+      </span>
+      <caption>Lorem ipsum dolor sit amet.</caption>
+      <small>Lorem ipsum dolor sit.</small>
+      <big>Lorem ipsum dolor sit amet.</big>
+    </div>
+  );
+};
+
+export default App;
