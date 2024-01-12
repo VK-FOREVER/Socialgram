b5f3b11e73149645e7d74a8d6d5723177e806d0d
Aman <dv451197@gmail.com>
2024-01-12 21:58:18 +0530

    fix config.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 const App = () => {
   return (
     <div>
-      <h1 className="text-slate-500 text-xl">
+      <h1 className="text-white text-xl">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quidem
         illo maiores, libero perferendis cupiditate quaerat ipsam ipsum
         laudantium neque eos, iure, at nobis voluptatum. Quisquam quasi facilis
