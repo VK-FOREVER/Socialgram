6c4be1febb960daeb3dfa3002d8bc7bf3e803e3f
Aman <dv451197@gmail.com>
2024-01-12 21:54:33 +0530

    adding tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
+import "./globals.css";
+
 const App = () => {
   return (
     <div>
-      <h1>
+      <h1 className="text-slate-500 text-xl">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quidem
         illo maiores, libero perferendis cupiditate quaerat ipsam ipsum
         laudantium neque eos, iure, at nobis voluptatum. Quisquam quasi facilis
         eum quaerat.
       </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-        ducimus earum, exercitationem ratione vitae error doloribus
-        necessitatibus aperiam odit mollitia?
-      </p>
-      <span>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, sed.
-      </span>
-      <caption>Lorem ipsum dolor sit amet.</caption>
-      <small>Lorem ipsum dolor sit.</small>
-      <big>Lorem ipsum dolor sit amet.</big>
     </div>
   );
 };
