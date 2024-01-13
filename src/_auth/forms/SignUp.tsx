a687e4c9cf41f3527bb23b2e0ac346be1c7e85c9
Aman <dv451197@gmail.com>
2024-01-13 15:55:13 +0530

    adding form

--- src/_auth/forms/SignUp.tsx
+++ src/_auth/forms/SignUp.tsx
@@ -14,17 +14,20 @@ import {
 import { Input } from "@/components/ui/input";
 import { SignUpValidation } from "@/lib/validation";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+// const formSchema = z.object({
+//   username: z.string().min(2, {
+//     message: "Username must be at least 2 characters.",
+//   }),
+// });
 
 const SignUp = () => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(SignUpValidation),
     defaultValues: {
+      name: "",
+      email: "",
+      password: "",
       username: "",
     },
   });
@@ -37,31 +40,106 @@ const SignUp = () => {
   }
 
   return (
-    <div>
+    <>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" style={{ background: "#fff", color: "gray" }}>
-            Submit
-          </Button>
-        </form>
+        <div className="sm:w-420 flex-center flex-col">
+          <img src="/assets/images/logo.svg" alt="Social Gram" />
+          <h2 className="h3-bold md:h2-bold pt-2 sm:pt-12">
+            Create an account
+          </h2>
+          <p className="text-light-3 small-medium md:base-regular mt-8 ">
+            Enter your details to join SocialGram
+          </p>
+
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-5 w-full mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Name"
+                      {...field}
+                      className="shad-input"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-light-2 tiny-medium">
+                    Enter your Name
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Username:"
+                      {...field}
+                      className="shad-input"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-light-2 tiny-medium">
+                    Enter your unique Username
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Email:"
+                      {...field}
+                      className="shad-input"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-light-2 tiny-medium ">
+                    your@email.com
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password:</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Password:"
+                      {...field}
+                      className="shad-input"
+                    />
+                  </FormControl>
+                  <FormDescription className="text-light-2 tiny-medium ">
+                    between 8 to 12 characters
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </div>
       </Form>
-    </div>
+    </>
   );
 };
 
