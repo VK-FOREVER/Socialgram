13363cda57b07594803da3b8caa8d00f2aeb49ac
Aman <dv451197@gmail.com>
2024-01-13 21:42:39 +0530

    adding appwrite

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,17 @@
-export  async function createUserAccount(user){
+import { INewUser } from "@/types";
+import { account } from "./config";
+import { ID } from "appwrite";
 
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.name,
+      user.password
+    );
+    return newAccount;
+  } catch (error) {
+    console.log(error);
+  }
 }
