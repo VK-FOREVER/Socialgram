b56a191eedcd5549db0c805d0fcc621e7071f07f
Aman <dv451197@gmail.com>
2024-01-29 14:56:09 +0530

    working on Home

---
+++ src/components/shared/PostCard.tsx
@@ -0,0 +1,77 @@
+import { useUserContext } from "@/context/AuthContext";
+import { timeAgo } from "@/lib/utils";
+import { Models } from "appwrite";
+import React from "react";
+import { Link } from "react-router-dom";
+
+type PostCardProps = {
+  post: Models.Document;
+};
+
+const PostCard = ({ post }: PostCardProps) => {
+  const { user } = useUserContext();
+  if (!post.creator) return;
+
+  return (
+    <div className="post-card">
+      <div className="flex-between">
+        <div className="flex items-center gap-3 ">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={post?.creator?.imageUrl}
+              alt={post?.caption}
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
+              {post.creator.name}
+            </p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">
+                {" "}
+                {post.$createdAt && timeAgo(post.$createdAt)}
+              </p>
+              -
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
+            </div>
+          </div>
+        </div>
+        {user.id === post.creator.$id && (
+          <Link to={`/update-post/${post.$id}`}>
+            <img
+              src="/assets/icons/edit.svg"
+              alt="Edit Post"
+              width={20}
+              height={20}
+            />
+          </Link>
+        )}
+      </div>
+      <Link to={`/posts/${post.$id}`}>
+        <div className="small-medium lg:base-medium py-5">
+          <p className="">{post.caption}</p>
+          <ul className="flex gap-1 mt-2">
+            {post.tag.map((t: string) => (
+              <li key={t} className="text-light-3">
+                #{t}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <img
+          src={
+            post.imageUrl
+              ? post.imageUrl
+              : "/assets/icons/profile-placeholder.svg"
+          }
+          alt={post.cation}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default PostCard;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,36 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Simplify the Date Format
+export function timeAgo(dateString: string): string {
+  const currentDate = new Date();
+  const pastDate = new Date(dateString);
+
+  const timeDifference = currentDate.getTime() - pastDate.getTime();
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+
+  if (minutes < 1) {
+    return `${seconds} seconds ago`;
+  } else if (minutes === 1) {
+    return "1 minute ago";
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else {
+    const hours = Math.floor(minutes / 60);
+    if (hours < 24) {
+      return `${hours} hours ago`;
+    } else {
+      const days = Math.floor(hours / 24);
+      return `${days} days ago`;
+    }
+  }
+}
+
+// const dateStr = "2024-01-28T12:56:32.492+00:00";
+// const formattedTime = timeAgo(dateStr);
+// // console.log(formattedTime);
