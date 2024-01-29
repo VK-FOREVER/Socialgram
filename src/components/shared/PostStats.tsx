a1d0bf6bf8ab33604c51c4b66e4053fafbe5e03f
Aman <dv451197@gmail.com>
2024-01-29 16:22:56 +0530

    implementing like feature

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -9,7 +9,7 @@ type PostStatsProps = {
 const PostStats = ({ post, userId }: PostStatsProps) => {
   return (
     <div className="flex justify-between items-center z-20">
-      <div className="flex gap-2 mr-5">
+      <div className="flex gap-2 mr-5 items-center justify-center">
         <img
           src="/assets/icons/like.svg"
           alt="like"
@@ -18,7 +18,17 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           onClick={() => {}}
           className="cursor-pointer"
         />
-        <p>0</p>
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+      <div className="flex gap-2 items-center justify-center">
+        <img
+          src="/assets/icons/save.svg"
+          alt="like"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
       </div>
     </div>
   );
