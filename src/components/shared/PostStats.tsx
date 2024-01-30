db1f89f17a15062e393089b3606220efc49f5488
Aman <dv451197@gmail.com>
2024-01-30 10:16:00 +0530

    svg change after an action

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -4,8 +4,9 @@ import {
   useLikePost,
   useSavePost,
 } from "@/lib/react-query/queriesAndMutations";
+import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
-import  { useState } from "react";
+import { useState } from "react";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -31,22 +32,26 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5 items-center justify-center">
         <img
-          src="/assets/icons/like.svg"
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "assets/icons/liked.svg"
+              : "assets/icons/like.svg"
+          }`}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleLikePosts}
           className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">0</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
       <div className="flex gap-2 items-center justify-center">
         <img
-          src={`${}/assets/icons/save.svg`}
+          src={isSaved ? "assets/icons/saved.svg" : "assets/icons/save.svg"}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleSavePosts}
           className="cursor-pointer"
         />
       </div>
