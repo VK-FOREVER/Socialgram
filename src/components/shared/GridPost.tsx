d01f94c58847ed82117d3e60411423026f04133e
Aman <dv451197@gmail.com>
2024-02-10 23:37:48 +0530

    fix post width

--- src/components/shared/GridPost.tsx
+++ src/components/shared/GridPost.tsx
@@ -23,7 +23,7 @@ const GridPost = ({
         <li
           key={post.$id}
           className={`relative ${
-            lowWidth ? `min-w-72 min-h-72` : `min-w-80 h-80`
+            lowWidth ? `min-w-72 min-h-60` : `min-w-80 h-80`
           }`}
         >
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
