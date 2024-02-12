4acb9189189107dd0457e56ca44a40db410b6f6b
Aman <dv451197@gmail.com>
2024-02-12 23:56:39 +0530

    adding queries

--- src/components/shared/TopCreators.tsx
+++ src/components/shared/TopCreators.tsx
@@ -1,7 +1,13 @@
 import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
+import Loader from "./Loader";
 
 const TopCreators = () => {
-  const {} = useGetUsers(12);
+  const { data: creators, isFetching: loading } = useGetUsers(12);
+  if (loading) {
+    return <Loader />;
+  }
+  //   console.log(creators);
+
   return <div className=""></div>;
 };
 
