486eb708edbbf00e031a84bab0bb1438fcdaa68f
Aman <dv451197@gmail.com>
2024-02-14 16:37:51 +0530

    fix type error

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,19 +1,17 @@
-import { Models } from "appwrite";
-import React from "react";
 import Loader from "./Loader";
 import GridPost from "./GridPost";
 
 type SearchTypesProps = {
   searching: boolean;
-  searchedPosts: Models.Document[];
+  searchedPosts: any;
 };
 
 const SearchResults = ({ searching, searchedPosts }: SearchTypesProps) => {
   if (searching) {
     return <Loader />;
   }
-  if (searchedPosts && searchedPosts.documents.length > 0) {
-    return <GridPost posts={searchedPosts.documents} />;
+  if (searchedPosts && searchedPosts?.documents.length > 0) {
+    return <GridPost posts={searchedPosts?.documents} />;
   }
   return (
     <div className="w-full text-center mt-10">
