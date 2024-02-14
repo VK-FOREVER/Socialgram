74db4c9b1ae9459f9abb8b6f32586186d636997c
Aman <dv451197@gmail.com>
2024-02-14 15:18:50 +0530

    allmost done

--- src/components/shared/TopCreators.tsx
+++ src/components/shared/TopCreators.tsx
@@ -2,13 +2,15 @@ import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
 import Loader from "./Loader";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { useUserContext } from "@/context/AuthContext";
 
 const TopCreators = () => {
   const { data: creators, isFetching: loading } = useGetUsers(12);
+  const { user } = useUserContext();
+
   if (loading) {
     return <Loader />;
   }
-
   return (
     <div className="home-creators">
       <h1 className="text-light-1 text-2xl mb-4">Top Creators</h1>
@@ -25,12 +27,14 @@ const TopCreators = () => {
                 {creator.name}
               </h4>
               <span className="text-light-3 text-xs">@{creator.username}</span>
-              <Button
-                variant="outline"
-                className="bg-primary-600 text-sm px-3 "
-              >
-                follow
-              </Button>
+              {creator.$id !== user.id && (
+                <Button
+                  variant="outline"
+                  className="bg-primary-600 text-sm px-3 h-8 mt-2"
+                >
+                  Follow
+                </Button>
+              )}
             </div>
           </Link>
         ))}
