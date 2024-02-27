2c0ee45f4d51e986c8a8d2d83385c575f4934a35
Aman <dv451197@gmail.com>
2024-02-27 17:21:30 +0530

    cleanup and new logo added

--- src/_auth/forms/SignIn.tsx
+++ src/_auth/forms/SignIn.tsx
@@ -74,8 +74,8 @@ const SignIn = () => {
         <div className="sm:w-450 flex-center flex-col bg-[#00000080] w-[500px] p-9 rounded-3xl">
           <div className="flex flex-1 items-center justify-center flex-col w-full pt-4 ">
             <img
-              src="/assets/images/socialgram.png"
-              className=""
+              src="/assets/images/socialgram1.png"
+              className="w-[200px]"
               alt="Social Gram"
             />
             <h2 className="h3-bold md:h2-bold pt-6 md:pt-4">
