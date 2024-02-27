b8ceb1fd8adeb12e974ef94baaf463b9c2c3d730
Aman <dv451197@gmail.com>
2024-02-27 11:39:33 +0530

    updated

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,4 @@
 import { ID, Query } from "appwrite";
-
 import { appwriteConfig, account, databases, storage, avatars } from "./config";
 import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 
