730625a85bfb7ebe22e42bb1da8d4ef0ca949312
Aman <dv451197@gmail.com>
2024-05-12 23:33:00 +0530

    Fixing FileUpload component structor

--- src/components/shared/FileUpload.tsx
+++ src/components/shared/FileUpload.tsx
@@ -5,61 +5,62 @@ import { FileUploaderProps } from "@/types";
 
 //Fix it
 const FileUpload = () => {
-  const [files, setFiles] = useState([])
+  const [files, setFiles] = useState([]);
   const options = {
     apiKey: "free", // Get API keys from: www.bytescale.com
     maxFileCount: 1,
     styles: {
       colors: {
-        primary: "#377dff"
-      }
-    }
-  }
+        primary: "#377dff",
+      },
+    },
+  };
 
-  const MyUploadButton = ({setFiles}: any) =>
-    <UploadButton options={options}
-                  onComplete={setFiles}>
-      {({onClick}) =>
-        <button onClick={onClick}>
-          Upload a file...
-        </button>
-      }
+  const MyUploadButton = ({ setFiles }: any) => (
+    <UploadButton options={options} onComplete={setFiles}>
+      {({ onClick }) => <button onClick={onClick}>Upload a file...</button>}
     </UploadButton>
-
-
-const MyUploadedFiles = ({files}: any) => files.map((file: any) => {
-  // Save 'filePath' to your DB, and construct URLs using UrlBuilder:
-  const { filePath, accountId } = file;
-  // Build an image transformation URL for the uploaded file. 
-  // Remove 'options' to get the URL to the original file:
-  const fileUrl = UrlBuilder.url({ 
-    filePath, 
-    accountId, 
-    options: { 
-      transformation: "preset", 
-      transformationPreset: "thumbnail"
-    }
-  });
-  return (
-    <p key={fileUrl}>
-      <a href={fileUrl} target="_blank">{fileUrl}</a>
-    </p>
   );
-})}
 
-    return (
-      <>
-        {files.length 
-           ? <MyUploadedFiles files={files} /> 
-           : <MyUploadButton setFiles={setFiles} />
-        }
-        <a className="developed_by" href="https://www.bytescale.com/docs/upload-widget/react" target="_blank">
-          Powered by Bytescale
-        </a>
-      </>
-    );
+  const MyUploadedFiles = ({ files }: any) =>
+    files.map((file: any) => {
+      // Save 'filePath' to your DB, and construct URLs using UrlBuilder:
+      const { filePath, accountId } = file;
+      // Build an image transformation URL for the uploaded file.
+      // Remove 'options' to get the URL to the original file:
+      const fileUrl = UrlBuilder.url({
+        filePath,
+        accountId,
+        options: {
+          transformation: "preset",
+          transformationPreset: "thumbnail",
+        },
+      });
+      return (
+        <p key={fileUrl}>
+          <a href={fileUrl} target="_blank">
+            {fileUrl}
+          </a>
+        </p>
+      );
+    });
 
+  return (
+    <>
+      {files.length ? (
+        <MyUploadedFiles files={files} />
+      ) : (
+        <MyUploadButton setFiles={setFiles} />
+      )}
+      <a
+        className="developed_by"
+        href="https://www.bytescale.com/docs/upload-widget/react"
+        target="_blank"
+      >
+        Powered by Bytescale
+      </a>
+    </>
+  );
 };
-  
 
 export default FileUpload;
