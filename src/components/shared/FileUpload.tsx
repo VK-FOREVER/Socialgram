d02d29e8ca901c4401f38241a28637dc91c33b77
Aman <dv451197@gmail.com>
2024-05-12 23:30:41 +0530

    fixing missing "{"

--- src/components/shared/FileUpload.tsx
+++ src/components/shared/FileUpload.tsx
@@ -1,104 +1,65 @@
-import { UploadDropzone } from "@bytescale/upload-widget-react";
+import { UploadButton, UploadDropzone } from "@bytescale/upload-widget-react";
 import { useState } from "react";
 import { UrlBuilder } from "@bytescale/sdk";
 import { FileUploaderProps } from "@/types";
 
 //Fix it
-const FileUpload = ({ fileUrl, fieldChange }: FileUploaderProps) => {
-  const [file, setFile] = useState<Array<string>>([]);
-  const [url, setUrl] = useState<string>(fileUrl || "");
-
+const FileUpload = () => {
+  const [files, setFiles] = useState([])
   const options = {
-    apiKey: "free",
+    apiKey: "free", // Get API keys from: www.bytescale.com
     maxFileCount: 1,
-    showFinishButton: true,
-
     styles: {
       colors: {
-        primary: "#377dff",
-      },
-    },
-  };
-
-  const MyDropzone = ({ setFile }: any) => (
-    <UploadDropzone
-      options={options}
-      onUpdate={({ uploadedFiles }: any) => setFile(uploadedFiles[0].fileUrl)}
-      onComplete={setFile}
-      width="600px"
-      height="375px"
-    />
-  );
-
-  // -----------------------------
-  // Display the uploaded files...
-  // -----------------------------
-
-  const MyUploadedFiles = ({ files }: any) =>
-    files.map((file: any) => {
-      // Save 'filePath' to your DB, and construct URLs using UrlBuilder:
-      const { filePath, accountId } = file;
-      // Build an image transformation URL for the uploaded file.
-      // Remove 'options' to get the URL to the original file:
-      const fileUrl = UrlBuilder.url({
-        filePath,
-        accountId,
-        options: {
-          transformation: "preset",
-          transformationPreset: "thumbnail",
-        },
-      });
-      setUrl(fileUrl);
-      {
-        console.log(fileUrl);
+        primary: "#377dff"
       }
-      return (
-        <p key={fileUrl}>
-          <a href={fileUrl} target="_blank">
-            The file URL{fileUrl}
-          </a>
-        </p>
-      );
-    });
+    }
+  }
 
-  // Code
-  // const options = {
-  //   apiKey: "free", // Get API keys from: www.bytescale.com
-  //   maxFileCount: 1
-  // };
+  const MyUploadButton = ({setFiles}: any) =>
+    <UploadButton options={options}
+                  onComplete={setFiles}>
+      {({onClick}) =>
+        <button onClick={onClick}>
+          Upload a file...
+        </button>
+      }
+    </UploadButton>
 
-  // function uploadFiles() {
-  //   Bytescale.UploadWidget.open(options).then(
-  //     files => alert(files.length === 0
-  //                    ? "No files selected."
-  //                    : `Files uploaded:\n\n${files.map(x => x.fileUrl).join("\n")}`),
-  //     error => alert(error)
-  //   );
-  // }
 
+const MyUploadedFiles = ({files}: any) => files.map((file: any) => {
+  // Save 'filePath' to your DB, and construct URLs using UrlBuilder:
+  const { filePath, accountId } = file;
+  // Build an image transformation URL for the uploaded file. 
+  // Remove 'options' to get the URL to the original file:
+  const fileUrl = UrlBuilder.url({ 
+    filePath, 
+    accountId, 
+    options: { 
+      transformation: "preset", 
+      transformationPreset: "thumbnail"
+    }
+  });
   return (
-    <>
-      {file.length > 0 ? (
-        <MyUploadedFiles className="w-full" files={file} />
-      ) : (
-        <MyDropzone className="w-full" setFiles={setFile} />
-      )}
-      <div className="w-full flex justify-center items-center p-4 flex-col">
-        {/* <img
-          className="w-2/3 h-2/4 rounded-lg bg-cover"
-          src={file[0]?.fileUrl}
-          alt="file"
-        /> */}
-        <a
-          className="developed_by  flex justify-start w-full my-2 text-sm"
-          href="https://www.bytescale.com/docs/upload-widget/react"
-          target="_blank"
-        >
+    <p key={fileUrl}>
+      <a href={fileUrl} target="_blank">{fileUrl}</a>
+    </p>
+  );
+})}
+
+    return (
+      <>
+        {files.length 
+           ? <MyUploadedFiles files={files} /> 
+           : <MyUploadButton setFiles={setFiles} />
+        }
+        <a className="developed_by" href="https://www.bytescale.com/docs/upload-widget/react" target="_blank">
           Powered by Bytescale
         </a>
-      </div>
-    </>
-  );
+      </>
+    );
+
 };
+  
 
 export default FileUpload;
