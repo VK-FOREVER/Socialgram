6e29267b7069471d3f8f4e421b74f7da085eab7e
Aman <dv451197@gmail.com>
2024-05-14 19:08:21 +0530

    remove comments

--- src/components/shared/FileUpload.tsx
+++ src/components/shared/FileUpload.tsx
@@ -35,10 +35,7 @@ const FileUpload = () => {
 
   const MyUploadedFiles = ({ files }: any) =>
     files.map((file: any) => {
-      // Save 'filePath' to your DB, and construct URLs using UrlBuilder:
       const { filePath, accountId } = file;
-      // Build an image transformation URL for the uploaded file.
-      // Remove 'options' to get the URL to the original file:
       const fileUrl = UrlBuilder.url({
         filePath,
         accountId,
