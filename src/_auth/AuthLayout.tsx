fb05f9f122972a3ef2d487a2f4c7c21d661c6e1e
Aman <dv451197@gmail.com>
2024-06-10 22:52:08 +0530

    removed video on authentication screen

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom";
-import Pictures from "/assets/video/pictures.mp4";
+// import Pictures from "/assets/video/pictures.mp4";
+import AuthBanner from "/assets/images/authentication.jpg";
 import { useUserContext } from "@/context/AuthContext";
 
 const Authlayout = () => {
@@ -11,16 +12,21 @@ const Authlayout = () => {
         <Navigate to="/" />
       ) : (
         <main className="relative w-full h-screen flex items-center justify-center flex-col ">
-          <section className="flex justify-center items-center flex-col absolute bg-[#00000010] w-full h-full z-10">
+          <section className="flex justify-center items-center flex-col absolute bg-[#00000055] w-full h-full z-10">
             <Outlet />
           </section>
 
-          <video
+          {/* <video
             src={Pictures}
             controls={false}
             autoPlay={true}
             loop
             muted
+            className="max-sm:w-[500px] xl:block  w-full  h-full  object-cover"
+          /> */}
+          <img
+            src={AuthBanner}
+            alt="Authentication Banner"
             className="max-sm:w-[500px] xl:block  w-full  h-full  object-cover"
           />
         </main>
